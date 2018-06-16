3f7817dfa3b8a2aa7f0b6895dbdc721fee5d0942
shridhar01_patil <shridhar.patil@accenture.com>
2018-06-16 21:54:59 +0100

    some clean up

--- Vagrantfile
+++ Vagrantfile
@@ -3,18 +3,22 @@
 
 Vagrant.configure(2) do |config| 
   config.vm.synced_folder ".", "/vagrant" 
-  config.vm.box = "bento/centos-7.1"
+  config.vm.box = "centos/7"
   config.vbguest.auto_update = false
   config.vm.network "private_network", ip: "192.168.33.20"
   config.vm.hostname = "terraform"
+
+
+  config.vm.provision "shell" do |s|
+      s.path = 'installation.sh'
+    end
+    
+
   config.vm.provision "ansible_local" do |ansible|
     ansible.verbose        = true
     ansible.raw_arguments  = ["-connection=local"]
     #ansible.galaxy_command = 'ansible-galaxy install rvm_io.ruby --roles-path=/home/vagrant/.ansible/roles --ignore-errors --force'
     #ansible.galaxy_command = 'ansible-galaxy install secfigo.terraform --roles-path=/home/vagrant/.ansible/roles --ignore-errors --force'
     ansible.playbook = "test.yml"
-  end
-  config.vm.provision :shell, inline: "ansible-galaxy install tommarshall.awscli --roles-path=/root/.ansible/roles --ignore-errors --force"
-  config.vm.provision :shell, inline: "ansible-galaxy install secfigo.terraform --roles-path=/root/.ansible/roles --ignore-errors --force"
-  config.vm.provision :shell, inline: "ansible-playbook --connection=local  /vagrant/playbook.yml"
+  end 
 end
--- installation.sh
+++ installation.sh
@@ -1,8 +1,39 @@
 #! /usr/bin/env bash
 
-mkdir -p /vagrant/test/integration/extensive_suite/controls
-touch /vagrant/test/integration/extensive_suite/inspec.yml
-touch /vagrant/test/integration/extensive_suite/controls/inspec_attributes.rb
-touch /vagrant/test/integration/extensive_suite/controls/operating_system.rb
-touch /vagrant/test/integration/extensive_suite/controls/reachable_other_host.rb
-touch /vagrant/test/integration/extensive_suite/controls/state_file.rb
+#mkdir -p /vagrant/test/integration/extensive_suite/controls
+#touch /vagrant/test/integration/extensive_suite/inspec.yml
+#touch /vagrant/test/integration/extensive_suite/controls/inspec_attributes.rb
+#touch /vagrant/test/integration/extensive_suite/controls/operating_system.rb
+#touch /vagrant/test/integration/extensive_suite/controls/reachable_other_host.rb
+#touch /vagrant/test/integration/extensive_suite/controls/state_file.rb
+
+echo updating package information
+yum update -y >/dev/null 2>&1
+
+echo installing development/networking tools and EPEL repos
+yum install -y net-tools build-essential epel-release  >/dev/null 2>&1
+yum install -y rubygems
+
+ 
+echo installing Ruby 2.4.4 via RVM
+
+if su - vagrant -c ' gpg2 --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3'; then
+   echo "key download successfuly"
+else 
+     echo "key download successfuly in 2nd attempt"
+    su - vagrant -c  'curl -sSL https://rvm.io/mpapis.asc | gpg2 --import'
+fi 
+
+
+
+su - vagrant -c 'curl -sSL https://get.rvm.io | bash -s stable'  # >/dev/null 2>&1
+su - vagrant -c 'rvm rvmrc warning ignore allGemfiles'  #>/dev/null 2>&1
+su - vagrant -c 'source $HOME/.rvm/scripts/rvm'  >> ~/.bash_profile
+su - vagrant -c 'rvm install "ruby-2.4.4"'
+
+
+echo "installing bundler"
+su - vagrant -c 'gem install bundler'  #>/dev/null 2>&1
+
+su - vagrant -c 'rvm use  2.4.4 --default'     
+su - vagrant -c 'gem install kitchen-terraform --version 3.3.1'
