6dff9e15702eebf83f9f2773c72b2cf8cf2d0011
shridhar01_patil <shridhar.patil@accenture.com>
2018-06-18 11:39:38 +0100

    deleted the copied test cases

--- installation.sh
+++ installation.sh
@@ -1,12 +1,5 @@
 #! /usr/bin/env bash
 
-#mkdir -p /vagrant/test/integration/extensive_suite/controls
-#touch /vagrant/test/integration/extensive_suite/inspec.yml
-#touch /vagrant/test/integration/extensive_suite/controls/inspec_attributes.rb
-#touch /vagrant/test/integration/extensive_suite/controls/operating_system.rb
-#touch /vagrant/test/integration/extensive_suite/controls/reachable_other_host.rb
-#touch /vagrant/test/integration/extensive_suite/controls/state_file.rb
-
 echo updating package information
 yum update -y >/dev/null 2>&1
 
